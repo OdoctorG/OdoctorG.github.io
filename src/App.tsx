198387d7098646a622be56d43a4d2562d1a1f77a
Greno <grenoscar@gmail.com>
2024-06-11 18:11:25 +0200

    update url

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import { AppData } from './AppData';
 
 
 function connect(): string{
-  var connect_string = "https://link.tink.com/1.0/transactions/connect-accounts?client_id=bfdc43265bac4643bed9e8607d47c170&redirect_uri=http://localhost:5173/callback&market=SE" 
+  var connect_string = "https://link.tink.com/1.0/transactions/connect-accounts?client_id=bfdc43265bac4643bed9e8607d47c170&redirect_uri=https://odoctorg.github.io/callback&market=SE" 
   //var connect_string = "https://link.tink.com/1.0/transactions/connect-accounts/?client_id=39e981ab00b042b0ae4731e51619079b&redirect_uri=http%3A%2F%2Flocalhost%3A5173%2Fcallback&market=SE&locale=en_US"
   connect_string +=  "&state=" + AppData.getSessionId()
   if (AppData.getAccessToken() != null){
-    connect_string = "http://localhost:5173/overview"
+    connect_string = "https://odoctorg.github.io/overview"
   }
   return connect_string
 }
