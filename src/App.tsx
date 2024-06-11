f5f2273282acd4c28d589be65f409b6ab5f5a391
Greno <grenoscar@gmail.com>
2024-06-11 20:11:18 +0200

    reroute to hash based addres

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { AppData } from './AppData';
 
 
 function connect(): string{
-  var connect_string = "https://link.tink.com/1.0/transactions/connect-accounts?client_id=bfdc43265bac4643bed9e8607d47c170&redirect_uri=https://odoctorg.github.io/#/callback&market=SE" 
+  var connect_string = "https://link.tink.com/1.0/transactions/connect-accounts?client_id=bfdc43265bac4643bed9e8607d47c170&redirect_uri=https://odoctorg.github.io/callback&market=SE" 
   //var connect_string = "https://link.tink.com/1.0/transactions/connect-accounts/?client_id=39e981ab00b042b0ae4731e51619079b&redirect_uri=http%3A%2F%2Flocalhost%3A5173%2Fcallback&market=SE&locale=en_US"
   connect_string +=  "&state=" + AppData.getSessionId()
   if (AppData.getAccessToken() != null){
